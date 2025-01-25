0bf2829e48e5aa91c62240c1e7bee4937e09fe26
Natalie Ike <natalie@sippd.com>
2025-01-25 15:25:52 -0800

    bonus

--- serial.py
+++ serial.py
@@ -18,5 +18,29 @@ class SerialGenerator:
 
     >>> serial.generate()
     100
+
+    >>> repr(serial)
+    'serial <SerialGenerator start=100 next=101>'
     """
 
+    def __init__(self, start):
+        self.start = start
+        self._current = start - 1 #first generated number should return start number
+
+    def __repr__(self):
+        return 'serial <SerialGenerator start={start} next={next}>'.format(start=self.start, next=self.next)
+
+    def generate(self):
+        self._current = self._current + 1
+        return self._current
+    
+    def reset(self):
+        self._current = self.start - 1
+
+    @property
+    def next(self):
+        return self._current + 1
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
